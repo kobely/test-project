428ebc19a6f021b5987437d95c6e08e7c3e18c7a
kobely <614347337@qq.com>
2023-07-26 23:05:35 +0800

    Update and rename main.ts to src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+c/import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
